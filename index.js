543a18ba69a526130928e2134623d102bcbf457b
cquintep <cquintep@emeal.nttdata.com>
2022-12-26 21:36:55 -0500

    subir archivos feature/dev_01

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function helloWorld() {
   console.log('Hello World from this npm package');
+  console.log('prueba 01 para feature/dev_01');
+  console.log('prueba 02 para feature/dev_01');
 }
 
 module.exports = helloWorld;
