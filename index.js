d5d3964a06d403e9a44e8da22e1407d775bdceab
cquintep <cquintep@emeal.nttdata.com>
2022-12-28 08:43:25 -0500

    prueba pipeline

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function helloWorld() {
-  console.log('Hello World from this npm package');
-  console.log('prueba 01 para feature/dev_01');
-  console.log('prueba 02 para feature/dev_01');
-  console.log('prueba feature/dev_02');
+  console.log('Hello World 2023');
   
 }
 
