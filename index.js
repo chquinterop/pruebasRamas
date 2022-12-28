1d4dc0b06e5ed2cefda2f0958f4b3d2fae5d93b3
chquinterop <105752227+chquinterop@users.noreply.github.com>
2022-12-28 11:19:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function helloWorld() {
 
   console.log('Hello World 2022/2023/2024/2025/CRISTIAN HHH');
-  console.log('Hello World 2022/2023/2024/2025/CRISTIAN HHH');
-  console.log('Hello World 2022/2023/2024/2025/CRISTIAN HHH');
 
 }
 
