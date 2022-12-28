1c1f1f07f1c3c435e422304cf0b72e9778965bf4
chquinterop <105752227+chquinterop@users.noreply.github.com>
2022-12-28 11:33:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function helloWorld() {
   console.log('Hello World from this npm package');
+  console.log('Hello World from this npm package');
+  console.log('Hello World from this npm package');
 }
 
 module.exports = helloWorld;
